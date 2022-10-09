6872ad1550140a59e06221ba3963707cdff88224
Javier Ferrer Sánchez-Barcaiztegui <javierferrersb@gmail.com>
2022-10-09 17:15:09 +0200

    Added text box and heading

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,41 @@
+import TextField from "@mui/material/TextField";
+import styles from "../styles/Search.module.css";
+import React from "react";
+import { IconButton, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useTheme } from "@mui/material/styles";
+
 function Search() {
+    const [search, setSearch] = React.useState("");
+    const theme = useTheme();
+
     return (
         <div>
-            <h1>Search</h1>
+            <Typography
+                className={styles.headText}
+                color={theme.palette.primary.dark}
+            >
+                Buscar productos...
+            </Typography>
+            <div className={styles.searchFieldArea}>
+                <TextField
+                    InputProps={{
+                        startAdornment: (
+                            <IconButton
+                                type="button"
+                                sx={{ p: "10px" }}
+                                aria-label="search"
+                            >
+                                <SearchIcon />
+                            </IconButton>
+                        ),
+                    }}
+                    fullWidth
+                    id="searchField"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                ></TextField>
+            </div>
         </div>
     );
 }
