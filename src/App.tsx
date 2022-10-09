19c77067b27136a1d125029130af8badf18c593c
Javier Ferrer Sánchez-Barcaiztegui <javierferrersb@gmail.com>
2022-10-09 13:51:08 +0200

    Added theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,27 @@ import { Routes, Route } from "react-router-dom";
 import InitialScreen from "./components/InitialScreen";
 import MainApp from "./components/MainApp";
 import "./styles/App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 function App() {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#00A458",
+            },
+            secondary: {
+                main: "#A4004D",
+            },
+        },
+    });
     return (
         <div className="App">
-            <Routes>
-                <Route path="/" element={<InitialScreen />} />
-                <Route path="*" element={<MainApp />} />
-            </Routes>
+            <ThemeProvider theme={theme}>
+                <Routes>
+                    <Route path="/" element={<InitialScreen />} />
+                    <Route path="*" element={<MainApp />} />
+                </Routes>
+            </ThemeProvider>
         </div>
     );
 }
