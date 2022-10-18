5f277444ed35eb4aa5fb6a12de2663ad31be5cf2
jorgemoreno <jorgemoreno@pop-os.localdomain>
2022-10-18 15:32:05 +0200

    finalized paginicial

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,81 +1,81 @@
-import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
-import HomeIcon from "@mui/icons-material/Home";
-import SearchIcon from "@mui/icons-material/Search";
-import ChatIcon from "@mui/icons-material/Chat";
-import MapIcon from "@mui/icons-material/Map";
-import ProfileIcon from "@mui/icons-material/AccountCircle";
-import { useState } from "react";
-import useReactPath from "../hooks/useReactPath";
+import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import ChatIcon from '@mui/icons-material/Chat';
+import MapIcon from '@mui/icons-material/Map';
+import ProfileIcon from '@mui/icons-material/AccountCircle';
+import { useState } from 'react';
+import useReactPath from '../hooks/useReactPath';
 
-import React from "react";
+import React from 'react';
 
 interface BottomNavProps {
-    navigateFunction: (value: string) => void;
+  navigateFunction: (value: string) => void;
 }
 
 function BottomNav(props: BottomNavProps) {
-    const isBuyer = localStorage.getItem("role") === "buyer";
-    const pathname = window.location.pathname; // in case user visits the path directly. The BottomNavBar is able to follow suit.
-    const path = useReactPath();
+  const isBuyer = localStorage.getItem('role') === 'buyer';
+  const pathname = window.location.pathname; // in case user visits the path directly. The BottomNavBar is able to follow suit.
+  const path = useReactPath();
 
-    React.useEffect(() => {
-        setValue(window.location.pathname);
-    }, [path]);
+  React.useEffect(() => {
+    setValue(window.location.pathname);
+  }, [path]);
 
-    const [value, setValue] = useState(pathname);
+  const [value, setValue] = useState(pathname);
 
-    const handleChange = (
-        event: React.SyntheticEvent<Element, Event>,
-        value: any
-    ) => {
-        setValue(value);
-    };
+  const handleChange = (
+    event: React.SyntheticEvent<Element, Event>,
+    value: any
+  ) => {
+    setValue(value);
+  };
 
-    return (
-        <Paper
-            sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-            elevation={3}
-        >
-            <BottomNavigation showLabels value={value} onChange={handleChange}>
-                <BottomNavigationAction
-                    label="Inicio"
-                    icon={<HomeIcon />}
-                    value="/home"
-                    onClick={() => props.navigateFunction("/home")}
-                />
-                {isBuyer && (
-                    <BottomNavigationAction
-                        label="Buscar"
-                        icon={<SearchIcon />}
-                        value="/search"
-                        onClick={() => props.navigateFunction("/search")}
-                    />
-                )}
-                {isBuyer && (
-                    <BottomNavigationAction
-                        label="Mapa"
-                        icon={<MapIcon />}
-                        value="/map"
-                        onClick={() => props.navigateFunction("/map")}
-                    />
-                )}
-                <BottomNavigationAction
-                    label="Chat"
-                    icon={<ChatIcon />}
-                    value="/chat"
-                    onClick={() => props.navigateFunction("chat")}
-                />
-                <BottomNavigationAction
-                    label="Perfil"
-                    icon={<ProfileIcon />}
-                    onClick={() => {
-                        props.navigateFunction("profile");
-                    }}
-                    value="/profile"
-                />
-            </BottomNavigation>
-        </Paper>
-    );
+  return (
+    <Paper
+      sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation showLabels value={value} onChange={handleChange}>
+        <BottomNavigationAction
+          label="Inicio"
+          icon={<HomeIcon />}
+          value="/home"
+          onClick={() => props.navigateFunction('/home')}
+        />
+        {isBuyer && (
+          <BottomNavigationAction
+            label="Buscar"
+            icon={<SearchIcon />}
+            value="/search"
+            onClick={() => props.navigateFunction('/search')}
+          />
+        )}
+        {isBuyer && (
+          <BottomNavigationAction
+            label="Mapa"
+            icon={<MapIcon />}
+            value="/map"
+            onClick={() => props.navigateFunction('/map')}
+          />
+        )}
+        <BottomNavigationAction
+          label="Chat"
+          icon={<ChatIcon />}
+          value="/chat"
+          onClick={() => props.navigateFunction('chat')}
+        />
+        <BottomNavigationAction
+          label="Perfil"
+          icon={<ProfileIcon />}
+          onClick={() => {
+            props.navigateFunction('profile');
+          }}
+          value="/profile"
+        />
+      </BottomNavigation>
+    </Paper>
+  );
 }
 
 export default BottomNav;
