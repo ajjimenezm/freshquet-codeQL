67449c5de73041134c2ccab7d352f7366b31bdeb
manueljishi <meeljcal@upv.edu.es>
2022-10-18 13:34:43 +0200

    product detail page rebase de main en mi rama

---
+++ src/components/Advertisements/AdDetail.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Advertisement from "../../types/Advertisement";
+
+function AdDetail() {
+  const { id } = useParams<{ id: string }>();
+  const [advertisement, setProduct] = useState<Advertisement>();
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    console.log(id);
+    // const getProduct = async () => {
+    //     try {
+    //         const { data } = await axios.get(`/api/products/${id}`);
+    //         setProduct(data);
+    //     } catch (err) {
+    //         setError(true);
+    //         alert(err);
+    //     }
+    // };
+    // getProduct();
+  }, [id]);
+
+  if (error) return <h1>Product not found</h1>;
+
+  return (
+    <>
+      {advertisement && (
+        <div>
+          <h1>{advertisement.name}</h1>
+          <h2>{advertisement.price}</h2>
+          <h3>{advertisement.description}</h3>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default AdDetail;
