bca4ed68b9d74840d6e9d931834e46e8a32ec369
Javier Ferrer Sánchez-Barcaiztegui <javierferrersb@gmail.com>
2022-10-20 09:40:17 +0200

    Improved style of search history

--- src/components/SearchHistoryElement.tsx
+++ src/components/SearchHistoryElement.tsx
@@ -11,11 +11,11 @@ function SearchHistoryElement(props: SearchHistoryElementProps) {
     };
     return (
         <div
-            className="mb-2 flex flex-row justify-between pt-2"
+            className="flex w-full cursor-default flex-row justify-between pb-2 pt-2 hover:bg-gray-100 active:bg-gray-400"
             onClick={handleClick}
         >
-            <div>{props.historyString}</div>
-            <div>
+            <div className="ml-2">{props.historyString}</div>
+            <div className="mr-2">
                 <HistoryIcon />
             </div>
         </div>
