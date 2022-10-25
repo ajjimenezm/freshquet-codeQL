9f3710c7c012ea9bd6bee88dd349111e9c853b55
Javier Ferrer Sánchez-Barcaiztegui <javierferrersb@gmail.com>
2022-10-25 11:30:07 +0200

    Removed extra bar from login

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,39 @@
-import React, { useContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import InitialScreen from './components/InitialScreen';
-import MainApp from './components/MainApp';
-import './styles/App.css';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import NewProduct from './components/advertisements/NewProduct';
-import { AuthContext } from './chatContext/AuthContext';
-import Login from './components/users/Login';
+import { Routes, Route } from "react-router-dom";
+import MainApp from "./components/MainApp";
+import "./styles/App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import NewProduct from "./components/advertisements/NewProduct";
+import Login from "./components/users/Login";
+import Register from "./components/users/Register";
 
 function App() {
-  
-  //const {currentUser} = useContext(AuthContext);
- 
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#63d4a1',
-      },
-      secondary: {
-        main: '#D46496',
-      },
-    },
-  });
+    //const {currentUser} = useContext(AuthContext);
 
-  const setRole = (roleName: string) => {
-    localStorage.clear();
-    localStorage.setItem('role', roleName);
-  };
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#63d4a1",
+            },
+            secondary: {
+                main: "#D46496",
+            },
+        },
+    });
 
-  return (
-    <div className="App">
-      <ThemeProvider theme={theme}>
-        <Routes>
-          {/* <Route path="/" element={<InitialScreen setRole={setRole} />} /> */}
-          <Route path="/" element={<MainApp />} />
-          <Route path="*" element={<MainApp />} />
-          <Route path="newproduct" element={<NewProduct />} />
-        </Routes>
-      </ThemeProvider>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ThemeProvider theme={theme}>
+                <Routes>
+                    {/* <Route path="/" element={<InitialScreen setRole={setRole} />} /> */}
+                    <Route path="/" element={<MainApp />} />
+                    <Route path="*" element={<MainApp />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="newproduct" element={<NewProduct />} />
+                </Routes>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
