4f4c1c93b99e86e384ad64010ebe8bb2e069366b
Javier Ferrer Sánchez-Barcaiztegui <javierferrersb@gmail.com>
2022-10-25 11:17:52 +0200

    Solved bug navbar login

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,39 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import InitialScreen from './components/InitialScreen';
-import MainApp from './components/MainApp';
-import './styles/App.css';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import NewProduct from './components/advertisements/NewProduct';
-import Login from './components/users/Login';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import InitialScreen from "./components/InitialScreen";
+import MainApp from "./components/MainApp";
+import "./styles/App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import NewProduct from "./components/advertisements/NewProduct";
+import Login from "./components/users/Login";
+import Register from "./components/users/Register";
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#63d4a1',
-      },
-      secondary: {
-        main: '#D46496',
-      },
-    },
-  });
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#63d4a1",
+            },
+            secondary: {
+                main: "#D46496",
+            },
+        },
+    });
 
-  const setRole = (roleName: string) => {
-    localStorage.clear();
-    localStorage.setItem('role', roleName);
-  };
-
-  return (
-    <div className="App">
-      <ThemeProvider theme={theme}>
-        <Routes>
-          {/* <Route path="/" element={<InitialScreen setRole={setRole} />} /> */}
-          <Route path="/" element={<MainApp />} />
-          <Route path="*" element={<MainApp />} />
-          <Route path="newproduct" element={<NewProduct />} />
-        </Routes>
-      </ThemeProvider>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ThemeProvider theme={theme}>
+                <Routes>
+                    {/* <Route path="/" element={<InitialScreen setRole={setRole} />} /> */}
+                    <Route path="/" element={<MainApp />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="*" element={<MainApp />} />
+                    <Route path="newproduct" element={<NewProduct />} />
+                </Routes>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -1,48 +1,46 @@
-import BottomNav from './BottomNav';
-import { useNavigate } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Map from './Map';
-import Search from './Search';
-import Chat from './Chat';
-import Login from './users/Login';
-import Register from './users/Register';
-import AdDetail from './advertisements/AdDetail';
-import Profile from './Profile/Profile';
-import SellerPage from './seller-page/SellerPage';
-import EditAdDetail from './advertisements/EditAdDetail';
-import { useEffect } from 'react';
+import BottomNav from "./BottomNav";
+import { useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Map from "./Map";
+import Search from "./Search";
+import Chat from "./Chat";
+import Login from "./users/Login";
+import Register from "./users/Register";
+import AdDetail from "./advertisements/AdDetail";
+import Profile from "./Profile/Profile";
+import SellerPage from "./seller-page/SellerPage";
+import EditAdDetail from "./advertisements/EditAdDetail";
+import { useEffect } from "react";
 
 function MainApp() {
-  //localStorage.clear();
+    //localStorage.clear();
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const user = localStorage.getItem('userToken');
-    if (!user) {
-      navigate('/login');
-    } else navigate('/home');
-  }, []);
+    useEffect(() => {
+        const user = localStorage.getItem("userToken");
+        if (!user) {
+            navigate("/login");
+        } else navigate("/home");
+    }, []);
 
-  return (
-    <>
-      <Routes>
-        <Route path="home" element={<Home />} />
-        <Route path="map" element={<Map />} />
-        <Route path="search" element={<Search />} />
-        <Route path="chat" element={<Chat />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="products/detail/:id" element={<AdDetail />} />
-        <Route path="seller/:id" element={<SellerPage />} />
-        <Route path="products/edit/:id" element={<EditAdDetail />} />
-       </Routes>
+    return (
+        <>
+            <Routes>
+                <Route path="home" element={<Home />} />
+                <Route path="map" element={<Map />} />
+                <Route path="search" element={<Search />} />
+                <Route path="chat" element={<Chat />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="products/detail/:id" element={<AdDetail />} />
+                <Route path="seller/:id" element={<SellerPage />} />
+                <Route path="products/edit/:id" element={<EditAdDetail />} />
+            </Routes>
 
-      <BottomNav navigateFunction={navigate} />
-    </>
-  );
+            <BottomNav navigateFunction={navigate} />
+        </>
+    );
 }
 
 export default MainApp;
