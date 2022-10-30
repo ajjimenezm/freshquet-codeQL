b391a0ef4948d33d5cf34893f705020936ef1a1a
ajjimenezm <ajimar2001@gmail.com>
2022-10-30 12:39:51 +0100

    avance chat

--- src/components/Chat/ChatMenu.tsx
+++ src/components/Chat/ChatMenu.tsx
@@ -1,10 +1,27 @@
-import React from "react"
+import React, { useEffect } from "react"
 import ChatCard from "./ChatCard"
 import MailIcon from '@mui/icons-material/Mail';
 import IconButton from '@mui/material/IconButton';
 import { common } from "@mui/material/colors";
+import {db} from "../../firebase";
+import { doc, onSnapshot } from "firebase/firestore";
+import { AuthContext } from '../../chatContext/AuthContext';
 
 function ChatMenu() {
+    const currentUser = React.useContext(AuthContext);
+
+    const [chats, setChats] = React.useState<any>([])
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(doc(db, "userChats", currentUser!.uid), (doc) => {
+            setChats(doc.data())
+        });
+        return unsubscribe
+    }, [currentUser!.uid])
+    
+    console.log(currentUser!.uid)
+    console.log(Object.entries(chats))
+
     return (
         <div className="chatMenu">
             <div className="chatInfo h-24 flex flex-row border-b-2 bg-emerald-100 items-center justify-center fixed top-0 left-0 right-0">
--- src/components/advertisements/AdDetail.tsx
+++ src/components/advertisements/AdDetail.tsx
@@ -7,7 +7,8 @@ import { Button, Rating } from '@mui/material';
 import {db} from "../../firebase";
 import { User } from "firebase/auth";
 import { AuthContext } from '../../chatContext/AuthContext';
-import {collection, getDocs, query, where} from "firebase/firestore";
+import {collection, getDocs, query, setDoc, where, doc, updateDoc, serverTimestamp, getDoc} from "firebase/firestore";
+import { serialize } from 'v8';
 
 function AdDetail() {
   const { id } = useParams<{ id: string }>();
@@ -16,7 +17,9 @@ function AdDetail() {
   const [userCategory, setUserCategory] = useState<string>();
   const [sellerId, setSellerId] = useState<string>();
   const [error, setError] = useState(false);
-  const [userChat, setUserChat] = useState<string>();
+  const [userChat, setUserChat] = React.useState<string>();
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [combinedId, setCombinedId] = React.useState<string>();
 
   const currentUser = React.useContext(AuthContext);
 
@@ -63,34 +66,69 @@ function AdDetail() {
   };
 
   const searchUserChat = async () => {
-    console.log(advertisement?.sellerId.username);
     const q = query(collection(db, "users"), where("displayName", "==", advertisement?.sellerId.username));
     const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
-          setUserChat(doc.id);
+        setUserChat(doc.id);
       });
     console.log(userChat);
   };
 
-  const navigateChat = async () => {
+  const createCombinedId = async () => {
     const uid = currentUser!.uid;
-    console.log(uid);
-    if (userChat == undefined) {
-      await searchUserChat();
-    }
-    const combinedId : unknown = 
-      uid > userChat!
-        ? uid + userChat
-        : userChat + uid;
+    uid > userChat!
+      ? setCombinedId(uid + userChat)
+      : setCombinedId(userChat + uid);
     console.log(combinedId);
-    
-    //const res = await getDocs(db, "chats", combinedId)
   };
 
-  const chatProcess = () => {
+  const chatProcess = async () => {
     searchUserChat();
+    createCombinedId();  
+  };
+
+  const navigateChat = async () => {
+    //navigate(`/chat/${userChat}`);
+    let exists = false;
+    const q = query(collection(db, "chats"));
+    const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+          if(doc.id === combinedId){
+              exists = true;
+          }
+      });
+    // const res = await getDoc(doc(db, "chats", combinedId!));
+    if(!exists){
+      await setDoc(doc(db, "chats", combinedId!), { messages: [] });
+
+      await updateDoc(doc(db, "userChats", currentUser!.uid), {
+        [combinedId!+".userInfo"]: {
+          uid : userChat,
+          displayName : advertisement?.sellerId.username,
+        },
+        [combinedId!+".date"]: serverTimestamp()
+      });
+      await updateDoc(doc(db, "userChats", userChat!), {
+        [combinedId!+".userInfo"]: {
+          uid : currentUser!.uid,
+          displayName : currentUser!.displayName,
+        },
+        [combinedId!+".date"]: serverTimestamp()
+      });
+    }
+  } ;
+
+  const buyProcess = () => {
+    setShowConfirm(true);
+    chatProcess();
+  };
+
+  const confirmBuy = () => {
+    chatProcess();
     navigateChat();
   };
+
+
 
   let edit: any;
   advertisement && userCategory === 'seller' && sellerId === userId
@@ -153,7 +191,7 @@ function AdDetail() {
               className="left-2"
               variant="outlined"
               color="primary"
-              onClick={() => chatProcess()}
+              onClick={() => buyProcess()}
             >
               Comprar
             </Button>
@@ -161,6 +199,50 @@ function AdDetail() {
           {edit}
         </div>
       )}
+      {showConfirm ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold justify-center">
+                    Confirmar compra
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowConfirm(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowConfirm(false)}
+                  >
+                    Atrás
+                  </button>
+                  <button
+                    className="bg-emerald-300 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => confirmBuy()}
+                  >
+                    Enviar mensaje
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </>
   );
 }
