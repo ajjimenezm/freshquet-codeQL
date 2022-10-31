bb7c7bd2c87779040e1089ce5cf08b47e8cf0603
Qu1qu3 <quiquegallifa01@gmail.com>
2022-10-31 14:20:54 +0100

    modificado interfaz de new product

--- src/components/advertisements/NewProduct.tsx
+++ src/components/advertisements/NewProduct.tsx
@@ -1,9 +1,18 @@
-import ComboBox from './Combobox';
-import React, { SyntheticEvent, useEffect, useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Category } from '../../types/Category';
-import Heading from '../Heading';
+import ComboBox from "./Combobox";
+import {
+  Button,
+  TextField,
+  FormHelperText,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+} from "@mui/material";
+import React, { SyntheticEvent, useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Category } from "../../types/Category";
+import Heading from "../Heading";
 
 interface NewProductsState {
   id: string;
@@ -19,20 +28,21 @@ export default function NewProducts() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const user = localStorage.getItem('userToken');
+    const user = localStorage.getItem("userToken");
     if (!user) {
-      navigate('/login');
+      navigate("/login");
     }
   }, []);
 
+  const [quantityError, setQuantityError] = useState(false);
   const [state, setState] = useState<NewProductsState>({
-    id: '',
-    name: '',
-    description: '',
+    id: "",
+    name: "",
+    description: "",
     pricePerKilogram: 0,
     category: Category.Fresh,
     averageReviewScore: 0.0,
-    sellerId: localStorage.getItem('userId') || '',
+    sellerId: localStorage.getItem("userId") || "",
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,6 +50,24 @@ export default function NewProducts() {
       ...state,
       [e.target.name]: e.target.value,
     });
+  };
+
+  const regexDecimalWithPoint = /^\d*\.?\d*$/;
+  const regexDecimalWithComma = /^\d*,?\d*$/;
+  const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    if (
+      value !== "" &&
+      (regexDecimalWithPoint.test(value) || regexDecimalWithComma.test(value))
+    ) {
+      setQuantityError(false);
+      setState({
+        ...state,
+        pricePerKilogram: parseFloat(value),
+      });
+    } else {
+      setQuantityError(true);
+    }
   };
 
   /*    const fileSelectedHandler = (event: any) => {
@@ -60,15 +88,15 @@ export default function NewProducts() {
 
   const handleSubmit = (event: { preventDefault: () => void }) => {
     event.preventDefault();
-    if (state.name === '') {
-      alert('Add a valid name to the product');
+    if (state.name === "") {
+      alert("Add a valid name to the product");
     } else if (
       state.pricePerKilogram <= 0 ||
       state.pricePerKilogram.toString().length === 0
     ) {
-      alert('Add a valid price for the product');
-    } else if (state.description === '') {
-      alert('Add a valid description to the product');
+      alert("Add a valid price for the product");
+    } else if (state.description === "") {
+      alert("Add a valid description to the product");
     } else {
       axios
         .post(
@@ -80,67 +108,70 @@ export default function NewProducts() {
         .then((res) => {
           console.log(res);
           if (res.status == 201) {
-            navigate('/home');
+            navigate("/home");
           }
         })
         .catch((res) => {
           //                console.log(res)
-          if (res.message == 'Network Error') alert('Network Error');
+          if (res.message == "Network Error") alert("Network Error");
         });
     }
   };
 
   return (
-    <div>
-      <Heading text="ADD NEW PRODUCT" />
-
-      <form onSubmit={handleSubmit}>
-        <label>Product name:</label>
-        <br />
-        <input
-          id="prod_name"
-          name="name"
-          type="text"
-          value={state.name}
-          onChange={handleChange}
-          className="mr-4 mb-4 w-full rounded-md p-2"
-        />
-        <br />
-        <label>Price per kilogram (€):</label>
-        <br />
-        <input
-          name="pricePerKilogram"
-          type="number"
-          value={state.pricePerKilogram}
-          onChange={handleChange}
-          className="mr-4 mb-4 w-full rounded-md p-2"
-        />
-        <br />
-        <label>Product description:</label>
-        <br />
-        <input
-          name="description"
-          type="text"
-          value={state.description}
-          onChange={handleChange}
-          className="mr-4 mb-4 w-full rounded-md p-2"
-        />
-        <br />
-        <label>Category:</label>
-        <br />
-        <ComboBox onChangeHandler={handleCategory} />
-        <br />
-        {/*
-                <input
-                    id="prod_img"
-                    name="prod_img"
-                    type="file"
-                    onChange={fileSelectedHandler}
-                />
-                <br />
-                */}
-        <button type="submit">ACEPTAR</button>
-      </form>
+    <div className="ml-7 mt-8">
+      <Heading text="AÑADIR NUEVO PRODUCTO" />
+      <div>
+        <div className="mb-8">
+          <TextField
+            name="name"
+            label="Nombre del producto"
+            onChange={handleChange}
+            id="fullWidth"
+            defaultValue={state.name}
+          />
+        </div>
+        <div className="mb-8">
+          <FormControl
+            sx={{ marginTop: 2 }}
+            variant="outlined"
+            error={quantityError}
+          >
+            <InputLabel htmlFor="quantity-field">Cantidad</InputLabel>
+            <OutlinedInput
+              id="quantity-field"
+              value={state.pricePerKilogram}
+              onChange={handlePriceChange}
+              endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+              aria-describedby="quantity-field-helper-text"
+              inputProps={{
+                "aria-label": "weight",
+              }}
+              label="Cantidad"
+            />
+            <FormHelperText id="quantity-field-helper-text">
+              {quantityError ? "Introduzca un número válido" : ""}
+            </FormHelperText>
+          </FormControl>
+        </div>
+        <div className="mb-8">
+          <ComboBox onChangeHandler={handleCategory} />
+        </div>
+        <div className="mb-8 mr-8">
+          <TextField
+            fullWidth
+            name="description"
+            label="Descripción del producto"
+            onChange={handleChange}
+            id="fullWidth"
+            defaultValue={state.description}
+            multiline
+          />
+        </div>
+        <Button className="mt-8" variant="outlined" onClick={handleSubmit}>
+          ACEPTAR
+        </Button>
+      </div>
     </div>
   );
 }
