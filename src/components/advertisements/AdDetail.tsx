640f4557180d6cac68609d977d87012247783ce2
manueljishi <manueljishi@gmail.com>
2022-11-02 10:39:42 +0100

    solucionado problema de ids undefined

--- src/components/advertisements/AdDetail.tsx
+++ src/components/advertisements/AdDetail.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import axios from 'axios';
-import Advertisement from '../../types/Advertisement';
-import { Button, Rating } from '@mui/material';
-import { db } from '../../firebase';
-import { User } from 'firebase/auth';
-import { AuthContext } from '../../chatContext/AuthContext';
-import {collection, getDocs, query, setDoc, where, doc, updateDoc, serverTimestamp, getDoc} from "firebase/firestore";
-import { serialize } from 'v8';
-import SimpleImageSlider from 'react-simple-image-slider';
+import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import Advertisement from "../../types/Advertisement";
+import { Button, Rating } from "@mui/material";
+import { db } from "../../firebase";
+import { User } from "firebase/auth";
+import { AuthContext } from "../../chatContext/AuthContext";
+import {
+  collection,
+  getDocs,
+  query,
+  setDoc,
+  where,
+  doc,
+  updateDoc,
+  serverTimestamp,
+  getDoc,
+} from "firebase/firestore";
+import { serialize } from "v8";
+import SimpleImageSlider from "react-simple-image-slider";
 
 function AdDetail() {
   const { id } = useParams<{ id: string }>();
@@ -20,7 +30,7 @@ function AdDetail() {
   const [error, setError] = useState(false);
   const [userChat, setUserChat] = React.useState<string>();
   const [showConfirm, setShowConfirm] = useState(false);
-  const [combinedId, setCombinedId] = React.useState<string>();
+  const [combinedId, setCombinedId] = React.useState<string>("");
   const [images, setImages] = useState<string[]>([]);
   const [imagenames, setImagenames] = useState<string[]>([]);
 
@@ -49,7 +59,6 @@ function AdDetail() {
             `${process.env.REACT_APP_BACKEND_DEFAULT_ROUTE}advertisements/${id}/images`
           )
           .then((res) => {
-            console.log(res);
             setImagenames(res.data);
           });
       } catch (err) {
@@ -66,14 +75,14 @@ function AdDetail() {
             .get(
               `${process.env.REACT_APP_BACKEND_DEFAULT_ROUTE}advertisements/${id}/images/${imagename}`,
               {
-                responseType: 'arraybuffer',
+                responseType: "arraybuffer",
               }
             )
             .then((res) => {
               setImages(
                 images.concat(
-                  `data:;base64,${Buffer.from(res.data, 'binary').toString(
-                    'base64'
+                  `data:;base64,${Buffer.from(res.data, "binary").toString(
+                    "base64"
                   )}`
                 )
               );
@@ -91,11 +100,10 @@ function AdDetail() {
   axios
     .get(`${process.env.REACT_APP_BACKEND_DEFAULT_ROUTE}users/type`, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('userToken')}`,
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
       },
     })
     .then((res) => {
-      console.log(res);
       setUserCategory(res.data.userRole);
       setUserId(res.data.userId);
     });
@@ -111,25 +119,27 @@ function AdDetail() {
   };
 
   const searchUserChat = async () => {
-    const q = query(collection(db, "users"), where("displayName", "==", advertisement?.sellerId.username));
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", advertisement?.sellerId.username)
+    );
     const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-        setUserChat(doc.id);
-      });
-    console.log(userChat);
+    querySnapshot.forEach((doc) => {
+      setUserChat(doc.id);
+      createCombinedId(doc.id);
+    });
+    //console.log(userChat);
   };
 
-  const createCombinedId = async () => {
+  const createCombinedId = async (id: string) => {
     const uid = currentUser!.uid;
-    uid > userChat!
-      ? setCombinedId(uid + userChat)
-      : setCombinedId(userChat + uid);
-    console.log(combinedId);
+    const newId = uid > id! ? uid + id : id + uid;
+    setCombinedId(newId);
   };
 
   const chatProcess = async () => {
     searchUserChat();
-    createCombinedId();  
+    //createCombinedId();
   };
 
   const navigateChat = async () => {
@@ -137,46 +147,49 @@ function AdDetail() {
     let exists = false;
     const q = query(collection(db, "chats"));
     const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-          if(doc.id === combinedId){
-              exists = true;
-          }
-      });
+    querySnapshot.forEach((doc) => {
+      if (doc.id === combinedId) {
+        exists = true;
+      }
+    });
     // const res = await getDoc(doc(db, "chats", combinedId!));
-    if(!exists){
+    if (!exists) {
       await setDoc(doc(db, "chats", combinedId!), { messages: [] });
 
       await updateDoc(doc(db, "userChats", currentUser!.uid), {
-        [combinedId!+".userInfo"]: {
-          uid : userChat,
-          displayName : advertisement?.sellerId.username,
+        [combinedId! + ".userInfo"]: {
+          uid: userChat,
+          displayName: advertisement?.sellerId.username,
         },
-        [combinedId!+".date"]: serverTimestamp()
+        [combinedId! + ".date"]: serverTimestamp(),
       });
       await updateDoc(doc(db, "userChats", userChat!), {
-        [combinedId!+".userInfo"]: {
-          uid : currentUser!.uid,
-          displayName : currentUser!.displayName,
+        [combinedId! + ".userInfo"]: {
+          uid: currentUser!.uid,
+          displayName: currentUser!.displayName,
         },
-        [combinedId!+".date"]: serverTimestamp()
+        [combinedId! + ".date"]: serverTimestamp(),
       });
     }
-  } ;
+  };
 
   const buyProcess = () => {
     setShowConfirm(true);
     chatProcess();
   };
 
+  React.useEffect(() => {
+    if (combinedId !== "") {
+      navigateChat();
+    }
+  }, [combinedId]);
+
   const confirmBuy = () => {
     chatProcess();
-    navigateChat();
   };
 
-
-
   let edit: any;
-  advertisement && userCategory === 'seller' && sellerId === userId
+  advertisement && userCategory === "seller" && sellerId === userId
     ? (edit = (
         <Button
           className="center-2"
@@ -210,7 +223,7 @@ function AdDetail() {
               images
                 ? images
                 : [
-                    'https://images.pexels.com/photos/144248/potatoes-vegetables-erdfrucht-bio-144248.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                    "https://images.pexels.com/photos/144248/potatoes-vegetables-erdfrucht-bio-144248.jpeg?auto=compress&cs=tinysrgb&w=1600",
                   ]
             }
           />
@@ -222,7 +235,7 @@ function AdDetail() {
             <p className="text-xl">{advertisement.description}</p>
           </div>
           <p className="py-4 text-2xl font-medium">
-            Categoría:{' '}
+            Categoría:{" "}
             <span className="border-1 rounded-lg p-2 uppercase">
               {advertisement.category}
             </span>
@@ -234,7 +247,7 @@ function AdDetail() {
             readOnly
           />
           <div className="mt-2 mb-4 flex flex-col items-center justify-center text-center">
-            Vendido por{' '}
+            Vendido por{" "}
             <span className="font-bold"> {advertisement.sellerId.name}</span>
             <Button
               className="left-2"
@@ -258,36 +271,34 @@ function AdDetail() {
       )}
       {showConfirm ? (
         <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
+            <div className="relative my-6 mx-auto w-auto max-w-3xl">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold justify-center">
+                <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
+                  <h3 className="justify-center text-3xl font-semibold">
                     Confirmar compra
                   </h3>
                   <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    className="float-right ml-auto border-0 bg-transparent p-1 text-3xl font-semibold leading-none text-black opacity-5 outline-none focus:outline-none"
                     onClick={() => setShowConfirm(false)}
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    <span className="block h-6 w-6 bg-transparent text-2xl text-black opacity-5 outline-none focus:outline-none">
                       ×
                     </span>
                   </button>
                 </div>
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                <div className="flex items-center justify-end rounded-b border-t border-solid border-slate-200 p-6">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="background-transparent mr-1 mb-1 px-6 py-2 text-sm font-bold uppercase text-red-500 outline-none transition-all duration-150 ease-linear focus:outline-none"
                     type="button"
                     onClick={() => setShowConfirm(false)}
                   >
                     Atrás
                   </button>
                   <button
-                    className="bg-emerald-300 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="mr-1 mb-1 rounded bg-emerald-300 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-emerald-600"
                     type="button"
                     onClick={() => confirmBuy()}
                   >
@@ -297,7 +308,7 @@ function AdDetail() {
               </div>
             </div>
           </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
         </>
       ) : null}
     </>
