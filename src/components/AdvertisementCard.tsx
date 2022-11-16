61542b319449dfeb65f8088a586ac63b4df6f14a
Javier Ferrer Sánchez-Barcaiztegui <javierferrersb@gmail.com>
2022-11-16 12:40:04 +0100

    Added loading skeleton for images

--- src/components/AdvertisementCard.tsx
+++ src/components/AdvertisementCard.tsx
@@ -1,50 +1,62 @@
-import { Button } from "@mui/material";
 import Advertisement from "../types/Advertisement";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import axios from "axios";
-import { Buffer } from "buffer";
-import { toNamespacedPath } from "path";
 import AdvertisementManagement from "../libs/AdvertisementManagement";
+import { Skeleton } from "@mui/material";
 
 interface AdvertisementCardProps {
-  advertisement: Advertisement;
+    advertisement: Advertisement;
 }
 
 function AdvertisementCard(props: AdvertisementCardProps) {
-  const navigate = useNavigate();
-  const [image, setImage] = useState<string>("");
+    const navigate = useNavigate();
+    const [imageLoaded, setImageLoaded] = useState(false);
+    const [image, setImage] = useState<string>("");
 
-  const navigateFunction = (id: string) => {
-    navigate(`/products/detail/${id}`);
-  };
+    const navigateFunction = (id: string) => {
+        navigate(`/products/detail/${id}`);
+    };
 
-  useEffect(() => {
-    AdvertisementManagement.GetImageAdvertisment(props.advertisement._id).then(
-      (res) => {
-        setImage(res);
-      }
-    );
-  });
+    useEffect(() => {
+        AdvertisementManagement.GetImageAdvertisment(
+            props.advertisement._id
+        ).then((res) => {
+            setImage(res);
+            setImageLoaded(true);
+        });
+    });
 
-  return (
-    <div
-      className="h-25 mt-1 mb-1 flex flex-row items-center"
-      onClick={navigateFunction.bind(null, props.advertisement._id)}
-    >
-      <img
-        src={image}
-        alt={props.advertisement.name}
-        className="aspect-square w-28 rounded-lg border-2 object-cover object-center"
-      />
-      <div className="ml-3 flex h-36 flex-col items-start justify-evenly pt-3 pb-3">
-        <div className="text-lg font-normal">{props.advertisement.name}</div>
-        <div className="text-sm">{props.advertisement.description}</div>
-        <div className="font-light">
-          {`${props.advertisement.pricePerKilogram} €/kg`}
+    return (
+        <div
+            className="h-25 mt-1 mb-1 flex flex-row items-center"
+            onClick={navigateFunction.bind(null, props.advertisement._id)}
+        >
+            {imageLoaded ? (
+                <img
+                    src={image}
+                    alt={props.advertisement.name}
+                    className="aspect-square w-28 rounded-lg border-2 object-cover object-center"
+                />
+            ) : (
+                <div className="aspect-square w-28 rounded-lg object-cover object-center">
+                    <Skeleton
+                        variant="rectangular"
+                        animation="wave"
+                        sx={{ width: 112, height: 112 }}
+                    />
+                </div>
+            )}
+
+            <div className="ml-3 flex h-36 flex-col items-start justify-evenly pt-3 pb-3">
+                <div className="text-lg font-normal">
+                    {props.advertisement.name}
+                </div>
+                <div className="text-sm">{props.advertisement.description}</div>
+                <div className="font-light">
+                    {`${props.advertisement.pricePerKilogram} €/kg`}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 export default AdvertisementCard;
