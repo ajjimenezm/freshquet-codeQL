924dfd7060117f106c9819e8e40f7717ca597d95
jorgemoreno <jorgemoreno@pop-os.localdomain>
2022-11-16 09:38:42 +0100

    valorar compra desde historial

--- src/components/advertismentHistory/advHistoryCardBuyer.tsx
+++ src/components/advertismentHistory/advHistoryCardBuyer.tsx
@@ -1,17 +1,23 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Advertisement from "../../types/Advertisement";
-import AdvertisementManagement from "../../libs/AdvertisementManagement";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Advertisement from '../../types/Advertisement';
+import AdvertisementManagement from '../../libs/AdvertisementManagement';
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 interface advHistoryCardProps {
+  compra_id: string;
   adv_id: string;
   quantity: number;
   is_ended: boolean;
+  review_text: string;
 }
 
 function AdvHistoryCard(props: advHistoryCardProps) {
+  const navigate = useNavigate();
+
   const [advertisement, setAdv] = useState<Advertisement>();
-  const [image, setImage] = useState<string>("");
+  const [image, setImage] = useState<string>('');
 
   useEffect(() => {
     axios
@@ -19,17 +25,25 @@ function AdvHistoryCard(props: advHistoryCardProps) {
         `${process.env.REACT_APP_BACKEND_DEFAULT_ROUTE}advertisements/${props.adv_id}`
       )
       .then((res) => {
-        console.log("RESPUESTA ADVERTISEMENT");
+        console.log('RESPUESTA ADVERTISEMENT');
         console.log(res);
         setAdv(res.data);
       });
-  });
 
-  useEffect(() => {
     AdvertisementManagement.GetImageAdvertisment(props.adv_id).then((res) => {
       setImage(res);
     });
-  });
+
+    console.log(props);
+  }, []);
+
+  const reviewPurchase = () => {
+    navigate(`/review/${props.compra_id}`);
+  };
+
+  const canReview = () => {
+    return props.is_ended && props.review_text === undefined;
+  };
 
   return (
     <div className="h-25 mt-1 mb-1 flex flex-row items-center">
@@ -48,6 +62,15 @@ function AdvHistoryCard(props: advHistoryCardProps) {
         <div>
           {props.is_ended ? <div>Finalizada</div> : <div>En tramite</div>}
         </div>
+      </div>
+      <div>
+        <Button
+          variant="contained"
+          onClick={() => reviewPurchase()}
+          disabled={!canReview()}
+        >
+          Deja una valoración
+        </Button>
       </div>
     </div>
   );
