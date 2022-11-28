207de2206962ec5c53dfa41becc35933df5f9a37
Javier Ferrer Sánchez-Barcaiztegui <javierferrersb@gmail.com>
2022-11-28 18:01:48 +0100

    Solucionado problemas no compilaba

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -1,72 +1,75 @@
-import PlaceReview from './reviews/PlaceReview';
-import BottomNav from './BottomNav';
-import { useNavigate } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Home/Home';
-import Map from './map/Map';
-import Chat from './Chat/Chat';
-import AdDetail from './advertisements/AdDetail';
-import Profile from './Profile/Profile';
-import SellerPage from './seller-page/SellerPage';
-import EditAdDetail from './advertisements/EditAdDetail';
-import { useEffect } from 'react';
-import ChatMenu from './Chat/ChatMenu';
+import PlaceReview from "./reviews/PlaceReview";
+import BottomNav from "./BottomNav";
+import { useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
+import Map from "./map/Map";
+import Chat from "./Chat/Chat";
+import AdDetail from "./advertisements/AdDetail";
+import Profile from "./Profile/Profile";
+import SellerPage from "./seller-page/SellerPage";
+import EditAdDetail from "./advertisements/EditAdDetail";
+import { useEffect } from "react";
+import ChatMenu from "./Chat/ChatMenu";
 
-import AdvertismentHistory from './advertismentHistory/advertismentHistory';
-import BuyerProfile from './Profile/BuyerProfile';
-import Search from './search/Search';
-import OrderCard from './Profile/OrderCard';
-import EditProfile from './Profile/EditProfile';
-import NearbyProducts from './Home/NearbyProducts';
+import AdvertismentHistory from "./advertismentHistory/advertismentHistory";
+import BuyerProfile from "./Profile/BuyerProfile";
+import Search from "./Search/Search";
+import OrderCard from "./Profile/OrderCard";
+import EditProfile from "./Profile/EditProfile";
+import NearbyProducts from "./Home/NearbyProducts";
 
 function MainApp() {
-  //localStorage.clear();
+    //localStorage.clear();
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const user = localStorage.getItem('userToken');
-    if (!user) {
-      navigate('/login');
-    }
-  }, []);
+    useEffect(() => {
+        const user = localStorage.getItem("userToken");
+        if (!user) {
+            navigate("/login");
+        }
+    }, []);
 
-  return (
-    <>
-      <Routes>
-        <Route path="home" element={<Home />} />
-        <Route path="map" element={<Map />} />
-        <Route path="search" element={<Search />} />
-        <Route path="chat" element={<Chat />} />
-        <Route path="chatmenu" element={<ChatMenu />} />
-        {/* <Route path="profile" element={<Profile />} /> */}
-        <Route path="profile" element={<BuyerProfile />} />
-        <Route path="editprofile" element={<EditProfile />} />
-        <Route path="products/detail/:id" element={<AdDetail />} />
-        <Route path="seller/:id" element={<SellerPage />} />
-        <Route path="products/edit/:id" element={<EditAdDetail />} />
-        <Route path="advertisementHistory" element={<AdvertismentHistory />} />
-        <Route path="review/:purchaseId" element={<PlaceReview />} />
-        <Route path="nearbyProducts" element={<NearbyProducts />} />
-        <Route
-          path="testest"
-          element={
-            <OrderCard
-              date="12/12/12"
-              is_ended={false}
-              price={1.66}
-              productName="Pimientos Verdes"
-              quantity={1}
-              sellerAddress="Casa"
-              sellerUsername="vendedor"
-            />
-          }
-        />
-      </Routes>
+    return (
+        <>
+            <Routes>
+                <Route path="home" element={<Home />} />
+                <Route path="map" element={<Map />} />
+                <Route path="search" element={<Search />} />
+                <Route path="chat" element={<Chat />} />
+                <Route path="chatmenu" element={<ChatMenu />} />
+                {/* <Route path="profile" element={<Profile />} /> */}
+                <Route path="profile" element={<BuyerProfile />} />
+                <Route path="editprofile" element={<EditProfile />} />
+                <Route path="products/detail/:id" element={<AdDetail />} />
+                <Route path="seller/:id" element={<SellerPage />} />
+                <Route path="products/edit/:id" element={<EditAdDetail />} />
+                <Route
+                    path="advertisementHistory"
+                    element={<AdvertismentHistory />}
+                />
+                <Route path="review/:purchaseId" element={<PlaceReview />} />
+                <Route path="nearbyProducts" element={<NearbyProducts />} />
+                <Route
+                    path="testest"
+                    element={
+                        <OrderCard
+                            date="12/12/12"
+                            is_ended={false}
+                            price={1.66}
+                            productName="Pimientos Verdes"
+                            quantity={1}
+                            sellerAddress="Casa"
+                            sellerUsername="vendedor"
+                        />
+                    }
+                />
+            </Routes>
 
-      <BottomNav navigateFunction={navigate} />
-    </>
-  );
+            <BottomNav navigateFunction={navigate} />
+        </>
+    );
 }
 
 export default MainApp;
